96677a401317f1b7721d00f4e6e31f643b97be59
Jordan Gonzalez <jgonzalez@streamlinx.com>
2015-08-31 22:35:24 -0400

    Adding to gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,54 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main( int argc, char* args[] ) {
-    SDL_Window* window = NULL;
-    SDL_Surface* screenSurface = NULL;
+bool init();
+bool loadMedia();
+void close();
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
+
+bool init() {
     if ( SDL_Init(SDL_INIT_VIDEO) < 0 ) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    } else {
-        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if (window == NULL) {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        } else {
-            screenSurface = SDL_GetWindowSurface(window);
-            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-            SDL_UpdateWindowSurface(window);
+        return false;
+    }
+    gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (gWindow == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return false;
+    }
+    gScreenSurface = SDL_GetWindowSurface(gWindow);
+    return true;
+}
+
+bool loadMedia() {
+    gHelloWorld = SDL_LoadBMP("res/hello_world.bmp");
+    if (gHelloWorld == NULL) {
+        printf("Unable to load image %s! SDL Error: %s\n", "res/hello_world.bmp", SDL_GetError());
+        return false;
+    }
+    return true;
+}
+
+void close() {
+    SDL_FreeSurface(gHelloWorld);
+    gHelloWorld = NULL;
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+    SDL_Quit();
+}
+
+int main( int argc, char* args[] ) {
+    if (init()) {
+        if (loadMedia()) {
+            SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+            SDL_UpdateWindowSurface(gWindow);
             SDL_Delay(2000);
         }
     }
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    close();
     return 0;
 }
+
